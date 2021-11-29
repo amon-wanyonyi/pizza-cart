1dd15385fa5f245826fb34311e5ad117a8bb6261
amon-wanyonyi <amon.wanyonyi@student.moringaschool.com>
2021-11-29 08:57:12 +0300

    Add screenshots and edit readme file

---
+++ README.md
@@ -0,0 +1,29 @@
+## AMOROM INN
+
+### Author: Amon Wanyonyi
+
+#### Description
+This web application for for Amorom Inn, where it allows the user to order for pizza, the user chooses the brand he/she wants, the selects the topping. <br>
+The application shows the user his/her cart items, the total cost to pay over the counter or on delivery. 
+
+![](assets/Screenshot.png)
+![](assets/Screenshot1.png)
+
+### Technologies Used
+-Bootstrap 5 <br>
+-CSS <br>
+-HTML <br>
+-JavaScript <br>
+-JQuery
+
+## Setup Instructios
+Download of git clone the repository <br>
+To git clone, Copy the repository URL <br>
+Open the terminal<br>
+Change the directory to the location you want <br>
+Type git clone, paste the URL you copies and hit enter
+
+## Contact Information
+For more information and questions, feel free to contact me through this mail
+xxx110680@gmail.com or mc.max.aw@gmail.com
+License
--- js/main.js
+++ js/main.js
@@ -1,188 +1,188 @@
+// Pizza objects
 function Pizza(size, crust) {
     this.size = size;
     this.crust = crust;
     this.toppings = [];
-  }
-  
-  function Location(name, estate) {
+}
+
+function Location(name, estate) {
     this.name = name;
     this.estate = estate;
-  }
-  
-  
-  var sizePrice = {
+}
+
+
+var sizePrice = {
     small: 600,
     medium: 800,
     large: 1200
-  };
-  var toppingPrice = [
-    {
-      pepperoni: {
+};
+var toppingPrice = [{
+    pepperoni: {
         small: 50,
         medium: 100,
         large: 150
-      },
-      veges: {
+    },
+    veges: {
         small: 25,
         medium: 50,
         large: 75
-      },
-      bacon: {
+    },
+    bacon: {
         small: 50,
         medium: 75,
         large: 100
-      }
     }
-  ];
-  
-  var crustPrice = {
+}];
+
+var crustPrice = {
     crispy: 300,
     stuffed: 200,
     gluten: 100
-  };
-  
-  function sizeCalcPrice(size) {
+};
+
+function sizeCalcPrice(size) {
     if (size === "small") {
-      return sizePrice.small * 1;
+        return sizePrice.small * 1;
     } else if (size === "medium") {
-      return sizePrice.medium * 1;
+        return sizePrice.medium * 1;
     } else {
-      return sizePrice.large * 1;
+        return sizePrice.large * 1;
     }
-  }
-  
-  function crustCalcPrice(crust) {
+}
+
+function crustCalcPrice(crust) {
     if (crust === "crispy") {
-      return crustPrice.crispy * 1;
+        return crustPrice.crispy * 1;
     } else if (crust === "stuffed") {
-      return crustPrice.stuffed * 1;
+        return crustPrice.stuffed * 1;
     } else {
-      return crustPrice.gluten * 1;
+        return crustPrice.gluten * 1;
     }
-  }
-  
-  function toppingsCalcPrice(toppings) {
+}
+
+function toppingsCalcPrice(toppings) {
     var noOfTopping = 0;
     for (i = 0; i < toppings.length; i++) {
-      if (toppings[i] == "pepperoni") {
-        noOfTopping += 100;
-      }
-      if (toppings[i] == "veges") {
-        noOfTopping += 50;
-      }
-      if (toppings[i] == "bacon") {
-        noOfTopping += 75;
-      }
+        if (toppings[i] == "pepperoni") {
+            noOfTopping += 100;
+        }
+        if (toppings[i] == "veges") {
+            noOfTopping += 50;
+        }
+        if (toppings[i] == "bacon") {
+            noOfTopping += 75;
+        }
     }
     return noOfTopping * 1;
-  }
-  
-  
-  function checkPepperoni(topping) {
-    return topping === "pepperoni";
-  }
-  
+}
 
-  $("document").ready(function() {
-    
+
+function checkPepperoni(topping) {
+    return topping === "pepperoni";
+}
+
+
+$("document").ready(function() {
+
     function getPizzaSize() {
-      return $("#pizza-size")
-        .find(":selected")
-        .val();
+        return $("#pizza-size")
+            .find(":selected")
+            .val();
     }
-    
+
     function getCrust() {
-      return $("#pizza-crust")
-        .find(":selected")
-        .val();
+        return $("#pizza-crust")
+            .find(":selected")
+            .val();
     }
-    
+
     function getToppings() {
-      var toppingList = [];
-      $(".toppings :checked").each(function() {
-        toppingList.push($(this).val());
-      });
-      return toppingList;
+        var toppingList = [];
+        $(".toppings :checked").each(function() {
+            toppingList.push($(this).val());
+        });
+        return toppingList;
     }
-  
-    
+
+
     $("form#myform").submit(function(event) {
-      event.preventDefault();
-      var pizzaSize = getPizzaSize();
-      var crust = getCrust();
-      var toppingList = getToppings();
-  
-      var newPizza = new Pizza(pizzaSize, crust);
-      newPizza.toppings.push(toppingList);
-      $("#cart").hide();
-      $("#table").show();
-      $(".checkout").show();
-      var oneOrder =
-        sizeCalcPrice(pizzaSize) +
-        crustCalcPrice(crust) +
-        toppingsCalcPrice(toppingList);
-  
-      
-      $("#items").append(
-        "<tr>" +
-          "<td>" +
-          newPizza.size +
-          "</td>" +
-          "<td>" +
-          "<p>" +
-          newPizza.crust +
-          "</p>" +
-          "</td>" +
-          "<td>" +
-          newPizza.toppings +
-          "</td>" +
-          "<td>" +
-          oneOrder +
-          "</td>" +
-          "</tr>"
-      );
+        event.preventDefault();
+        var pizzaSize = getPizzaSize();
+        var crust = getCrust();
+        var toppingList = getToppings();
+
+        var newPizza = new Pizza(pizzaSize, crust);
+        newPizza.toppings.push(toppingList);
+        $("#cart").hide();
+        $("#table").show();
+        $(".checkout").show();
+        var oneOrder =
+            sizeCalcPrice(pizzaSize) +
+            crustCalcPrice(crust) +
+            toppingsCalcPrice(toppingList);
+
+
+        $("#items").append(
+            "<tr>" +
+            "<td>" +
+            newPizza.size +
+            "</td>" +
+            "<td>" +
+            "<p>" +
+            newPizza.crust +
+            "</p>" +
+            "</td>" +
+            "<td>" +
+            newPizza.toppings +
+            "</td>" +
+            "<td>" +
+            oneOrder +
+            "</td>" +
+            "</tr>"
+        );
     });
     var totalQuantity = parseInt($("#quantity").val());
+
     function calcTotal() {
-      var priceOnePizza =
-        sizeCalcPrice(getPizzaSize()) +
-        crustCalcPrice(getCrust()) +
-        toppingsCalcPrice(getToppings());
-      return priceOnePizza;
+        var priceOnePizza =
+            sizeCalcPrice(getPizzaSize()) +
+            crustCalcPrice(getCrust()) +
+            toppingsCalcPrice(getToppings());
+        return priceOnePizza;
     }
     var pizzaList = [];
-    
+
     $("#orderbtn").on("click", function() {
-      totalQuantity += 1;
-      $("#quantity").text(totalQuantity);
-      pizzaList.push(calcTotal());
+        totalQuantity += 1;
+        $("#quantity").text(totalQuantity);
+        pizzaList.push(calcTotal());
     });
-  
-    
+
+
     $("#gettotal").click(function() {
-      var total = 0;
-      pizzaList.forEach(function(pizza) {
-        total += pizza;
-      });
-      $("#money").text(total);
+        var total = 0;
+        pizzaList.forEach(function(pizza) {
+            total += pizza;
+        });
+        $("#money").text(total);
     });
-  
-    
+
+
     $("#myModel").click(function() {
-      var deliver = confirm(
-        "Would you like us deliver your pizza to your Location? transport cost ksh 250."
-      );
-      if (deliver) {
-        var place = prompt("Enter your location");
-        $("#place").text(place);
-        $("#success").show();
-      } else {
-        $("#no-delivery").show();
-      }
-      
-      $("#pizza-size").val("");
-      $("#pizza-crust").val("");
-      $("#items").remove();
-      $("#quantity").text(0);
+        var deliver = confirm(
+            "Would you like us deliver your pizza to your Location? transport cost ksh 250."
+        );
+        if (deliver) {
+            var place = prompt("Enter your location");
+            $("#place").text(place);
+            $("#success").show();
+        } else {
+            $("#no-delivery").show();
+        }
+
+        $("#pizza-size").val("");
+        $("#pizza-crust").val("");
+        $("#items").remove();
+        $("#quantity").text(0);
     });
-  });
+});
